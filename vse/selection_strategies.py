f4dd99028eeb1f8f94ad8bc2701a5144e1f76d30
Bjørn Hoxmark <bhoxmark@gmail.com>
2018-03-12 12:56:47 +0100

    added 2 selections; All and caption similarity within a set

--- vse/selection_strategies.py
+++ vse/selection_strategies.py
@@ -1,11 +1,14 @@
 from model import cosine_sim
 from evaluation import encode_data
+import torch.nn as nn
 import random
 import torch
+from torch.autograd import Variable
 import heapq
 import scipy
 import copy
 import numpy as np
+import sys
 
 def select_random(r, model, train_loader):
     if r == 0:
@@ -56,6 +59,41 @@ def select_margin(r, model, train_loader, primary="image"):
         best_n_indices = [n[0] for n in heapq.nsmallest(128, enumerate(scores), key=lambda x: x[1])]
         return best_n_indices
 
+def select_captionSimilarity(r, model, train_loader, primary="image"):
+    if r == 0:
+        return random.sample(range(0, 30000), 1280)
+    else:    
+        model.val_start()
+        img_embs, cap_embs = encode_data(model, train_loader)
+        primary_embs, secondary_embs = (img_embs, cap_embs) if primary == "image" else (cap_embs, img_embs)
+
+        scores = []
+
+        for i in range(0, len(cap_embs), 5):
+            batch_range = min(5, len(cap_embs) - i)
+            primary_batch = cap_embs[i: i + batch_range]
+            primary_batch = Variable(torch.FloatTensor(primary_batch))
+            primary_batch_sum = get_avg_distance(primary_batch)
+            # print(primary_batch_sum)
+            scores.append(primary_batch_sum.data.cpu().numpy())
+
+        #Best results        
+        best_n_indices = [n[0] for n in heapq.nsmallest(128, enumerate(scores), key=lambda x: x[1])]    
+        print(best_n_indices)
+        #worst results
+        # best_n_indices = [n[0] for n in heapq.nlargest(128, enumerate(scores), key=lambda x: x[1])]
+
+        return best_n_indices
+
+def get_avg_distance(caption_set):
+    average_cap = 0.0
+    total_cap_vector = torch.sum(caption_set, dim=0)
+    average_cap_vector = torch.div(total_cap_vector,len(caption_set))
+    average_cap_vector.unsqueeze(0)
+    cosine_dist = average_cap_vector.unsqueeze(0).mm(caption_set.t())
+    average_distance = torch.sum(cosine_dist)
+    return average_distance
+
 def select_uncertainty(r, model, train_loader, primary="image"):
     if r == 0:
         return random.sample(range(0, 30000), 1280)
@@ -65,7 +103,7 @@ def select_uncertainty(r, model, train_loader, primary="image"):
         primary_embs, secondary_embs = (img_embs, cap_embs) if primary == "image" else (cap_embs, img_embs)
 
         scores = []
-
+        
         for i in range(0, len(primary_embs), 128):
             batch_range = min(128, len(primary_embs) - i)
             primary_batch = primary_embs[i: i + batch_range]
@@ -115,3 +153,11 @@ def select_hybrid(r, model, train_loader):
         return select_uncertainty(r, model, train_loader)
     else:
         return select_margin(r, model, train_loader)
+
+def select_all(r, model, train_loader):
+    if r == 0:   
+        size = ((len(train_loader)*128)-128)
+        # print(size)
+        return range(0, size) 
+    else: 
+        sys.exit("Done!")
