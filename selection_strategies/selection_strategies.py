Fixed a bug with w2v average word vectors

selection_strategies/selection_strategies.py
@@ -58,8 +58,7 @@ def select_egl(model, lg, iteration):
         feature = Variable(torch.LongTensor(batch_x))
         target = Variable(torch.LongTensor(batch_y))
         if params["CUDA"]:
-            feature, target = feature.cuda(
-                params["DEVICE"]), target.cuda(params["DEVICE"])
+            feature, target = feature.cuda(), target.cuda()
 
         for s_index in range(batch_range):
             score = 0
@@ -71,7 +70,7 @@ def select_egl(model, lg, iteration):
                 optimizer.zero_grad()
                 f_target = torch.autograd.Variable(torch.LongTensor([index]))
                 if params["CUDA"]:
-                    f_target = f_target.cuda(params["DEVICE"])
+                    f_target = f_target.cuda()
 
                 loss = criterion(output, f_target)
                 loss.backward(retain_graph=True)
@@ -134,8 +133,7 @@ def select_entropy(model, lg, iteration):
         target = Variable(torch.LongTensor(batch_y))
 
         if params["CUDA"]:
-            feature, target = feature.cuda(
-                params["DEVICE"]), target.cuda(params["DEVICE"])
+            feature, target = feature.cuda(), target.cuda()
 
         output = model(feature)
         # Output is not a probability distribution - make it using softmax
@@ -207,14 +205,14 @@ def clean(features, targets, indices):
             second = features[k]
 
             distance = getDistance(first, second, j, cachedFeatures);
-            print(distance)
-            #
-            # if distance < params["SIMILARITY_THRESHOLD"]:
-            #     to_delete.append(k)
-            #     # print("Distance: {}".format(distance))
-            #     # print(*[data["vocab"][i] for i in filter(lambda a: a < len(data["vocab"]), first)])
-            #     # print(*[data["vocab"][i] for i in filter(lambda a: a < len(data["vocab"]), second)])
-            #     # print("\n\n")
+            # print(distance)
+
+            if distance < params["SIMILARITY_THRESHOLD"]:
+                to_delete.append(k)
+                # print("Distance: {}".format(distance))
+                # print(*[data["vocab"][i] for i in filter(lambda a: a < len(data["vocab"]), first)])
+                # print(*[data["vocab"][i] for i in filter(lambda a: a < len(data["vocab"]), second)])
+                # print("\n\n")
 
     to_delete = list(set(to_delete))
 
@@ -261,8 +259,8 @@ def getDistance(first, second, j, savedFirsts):
 
     if params["SIMILARITY_REPRESENTATION"] == "W2V":
 
-        first_w2v = utils.average_feature_vector(first, w2v["w2v_kv"])
-        second_w2v = utils.average_feature_vector(second, w2v["w2v_kv"])
+        first_w2v = utils.average_feature_vector(first, w2v["w2v"])
+        second_w2v = utils.average_feature_vector(second, w2v["w2v"])
 
         distance = spatial.distance.cosine(first_w2v, second_w2v)
 
@@ -328,7 +326,7 @@ def batchify(features, params):
 
     for feature in features:
         if params["CUDA"]:
-            feature = feature.cuda(params["DEVICE"])
+            feature = feature.cuda()
 
         if(len(feature) < max_len):
             padding = [21426 for x in range(max_len - len(feature))]
@@ -336,7 +334,7 @@ def batchify(features, params):
             padding = torch.LongTensor(padding)
 
             if params["CUDA"]:
-                padding = padding.cuda(params["DEVICE"])
+                padding = padding.cuda()
             feature = torch.cat([feature, padding])
         else:
             feature = feature[0:max_len]

selection_strategies/utils.py
@@ -192,17 +192,15 @@ load word2vec pre trained vectors
 def load_word2vec():
     print("loading word2vec...")
     t0 = time.time()
-    
+
     word_vectors = KeyedVectors.load_word2vec_format(
         "{}/GoogleNews-vectors-negative300.bin".format(params['DATA_PATH']), binary=True)
-    
+
     t1 = time.time()
 
     total = t1-t0
 
     print("It took {} to load w2v".format(total))
-
-    # data["w2v_kv"] = word_vectors
 
     wv_matrix = []
     for word in data["vocab"]:
@@ -217,20 +215,13 @@ def load_word2vec():
     wv_matrix.append(np.zeros(300).astype("float32"))
     wv_matrix = np.array(wv_matrix)
     w2v["w2v"] = wv_matrix
-    w2v["w2v_kv"] = word_vectors
-    # return word_vectors, wv_matrix
 
-
-def average_feature_vector(sentence, embedding):
+def average_feature_vector(sentence, word_vectors):
     num_features = 300
     feature_vector = np.zeros((num_features, ), dtype="float32")
 
     for word in sentence:
-        # word_vector = embedding(word)
-        if word in embedding.vocab:
-            word_vector = embedding.word_vec(word)
-        else:
-            word_vector = np.random.uniform(-0.01, 0.01, num_features).astype("float32")
+        word_vector = word_vectors[word]
         feature_vector = np.add(feature_vector, word_vector)
 
     feature_vector = np.divide(feature_vector, len(sentence))