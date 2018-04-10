6f98e1de9a6c32e45d8643d4402932b90f46ef46
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2018-04-10 08:28:30 +0200

    Further testing + cleanup

--- hungarian/test.py
+++ hungarian/test.py
@@ -6,7 +6,6 @@ import numpy as np
 
 from scipy.optimize import linear_sum_assignment
 
-
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -19,51 +18,34 @@ class bcolors:
 
 
 n = 100
-
-caption_size = 100
-image_size = 4096
-embed_size = 1024
 epoch = 10000
 
 
 class LagrangeLoss(nn.Module):
     def __init__(self):
         super(LagrangeLoss, self).__init__()
-        # self.logsoftmax = nn.LogSoftmax(dim=1)
-        self.softmax = nn.Softmax(dim=1)
         self.criterion = nn.CrossEntropyLoss()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, distances, truth):
-        distances_sm = self.softmax(distances)
         err1 = self.criterion(distances, truth)
-        err2 = torch.sum(torch.abs(1 - torch.sum(distances_sm, 1)))
-        err2 *= 10000
+        err2 = torch.sum(torch.abs(1 - torch.sum(distances, 1)))
+        err3 = torch.sum(torch.abs(1 - torch.sum(distances, 0)))
+        # print(err2)
         return err1 + err2
-
 
 class Hungarian(nn.Module):
     def __init__(self):
         super(Hungarian, self).__init__()
-
-        self.fc_row = nn.Linear(n, n)
-        self.fc_col = nn.Linear(n, n)
-        self.fc = nn.Linear(2 * n, n)
+        self.softmax = nn.Softmax(dim=1)
+        self.fc = nn.Linear(n*n, n*n)
 
     def forward(self, input):
-        cat = torch.cat((self.fc_col(input), self.fc_row(input.transpose(1, 0))), 1)
-        out = self.fc(cat)
-
+        out = input.view(1, n * n)
+        # out = self.softmax(out)
+        out = self.fc(out)
+        out = out.view(n, n)
         return out
-
-
-model = Hungarian()
-
-if torch.cuda.is_available():
-    model.cuda()
-
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # lr=0.0002
-criterion = LagrangeLoss()
-
 
 def eval():
     model.eval()
@@ -72,21 +54,24 @@ def eval():
     if torch.cuda.is_available():
         distances = distances.cuda()
     distances_np = distances.data.cpu().numpy()
+
+    # Solve using the hungarian algorithm, and get the associated cost
     rows, hung_solution = linear_sum_assignment(distances_np)
     optimal_cost = distances_np[rows, hung_solution].sum()
 
+    # Get our
     feature = model(distances)
-
     target = torch.autograd.Variable(torch.LongTensor(hung_solution))
     if torch.cuda.is_available():
         target = target.cuda()
 
     loss = criterion(feature, target)
+
     corrects = (torch.max(feature, 1)[1].view(target.size()).data == target.data).sum()
 
+    # Get the cost of our predicted matrix
     distances_np_pred = feature.data.cpu().numpy()
     rows_pred, hung_solution_pred = linear_sum_assignment(distances_np_pred)
-
     cost_pred = distances_np[rows_pred, hung_solution_pred].sum()
 
     print("\n")
@@ -99,7 +84,7 @@ def eval():
     for row, index in enumerate(greedy_ass):
         greedy_cost += distances[row][index].data.cpu().numpy()[0]
 
-    approximate_assignment = greedy_assignment(feature, softmax=True)
+    approximate_assignment = greedy_assignment(feature)
     approximate_cost = 0
     for row, index in enumerate(approximate_assignment):
         approximate_cost += distances[row][index].data.cpu().numpy()[0]
@@ -110,10 +95,9 @@ def eval():
         greedy_cost, approximate_cost, optimal_cost))
 
 
-def greedy_assignment(cost_matrix, softmax=False):
+def greedy_assignment(cost_matrix, print_col=True):
+    # print(cost_matrix)
     num_collisions = 0
-    if softmax:
-        cost_matrix = torch.nn.functional.softmax(cost_matrix, dim=1)
     assignments = []
     cost_matrix = cost_matrix.data.cpu().numpy()
     for row in cost_matrix:
@@ -126,11 +110,13 @@ def greedy_assignment(cost_matrix, softmax=False):
             assignment = best_assignments[0]
 
         assignments.append(assignment)
-    print("NUM COLLISIONS: ", num_collisions)
+    if print_col:
+        print("NUM COLLISIONS: ", num_collisions)
     return assignments
 
 
 def check_bijection(assignment, solution, softmax=False):
+    softmax = False
     errors = []
     if softmax:
         assignment = torch.nn.functional.softmax(assignment)
@@ -161,6 +147,12 @@ def check_bijection(assignment, solution, softmax=False):
         line = ass + "\t|\t" + correct
         print(line)
 
+model = Hungarian()
+if torch.cuda.is_available():
+    model.cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # lr=0.0002
+criterion = LagrangeLoss()
 
 model.train()
 for i in range(epoch):
@@ -178,11 +170,15 @@ for i in range(epoch):
     if torch.cuda.is_available():
         target = target.cuda()
 
+    # print(feature)
+
     loss = criterion(feature, target)
     loss.backward()
     optimizer.step()
 
-    corrects = (torch.max(feature, 1)[1].view(target.size()).data == target.data).sum()
+    # corrects = (torch.max(feature, 1)[1].view(target.size()).data == target.data).sum()
+    approximate_assignment = greedy_assignment(feature, print_col=False)
+    corrects = sum([1 if approximate_assignment[i] == hung_solution[i] else 0 for i, _ in enumerate(approximate_assignment)])
     print("Epoch {:.4f} - loss {:.4f} - {:.4f} %".format(i,
                                                          loss.data[0], (corrects / n) * 100), end="\r")
 
