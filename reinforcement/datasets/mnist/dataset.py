67d1ec513f13993f0f98e28d55a5eef053cdeeec
Bjørn Hoxmark <bhoxmark@gmail.com>
2018-06-19 17:09:25 +0200

    Code clean 2

--- reinforcement/datasets/mnist/dataset.py
+++ reinforcement/datasets/mnist/dataset.py
@@ -1,7 +1,9 @@
 from sklearn import datasets, svm, metrics
 import sklearn
+import numpy as np
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import time
 
 
 from config import opt 
@@ -13,25 +15,27 @@ def load_data():
         test_set = dset.MNIST(root=opt.data_path , train=False, transform=trans, download=True)
 
         split = len(test_set)
-        x = []
-        y = []
 
+        ##Train dataset        
         x = train_set.train_data.cpu().numpy()
-        x = [i.ravel() for i in x]
+        x = np.ravel(x)
         y = train_set.train_labels.cpu().numpy()
-
         
+        ##Dev dataset
         x_dev = test_set.test_data.cpu().numpy()[:split]
-        x_dev = [i.ravel() for i in x_dev]
+        x_dev = np.ravel(x_dev)
         y_dev = test_set.test_labels.cpu().numpy()[:split]
         
+        ##Test dataset
         x_test = test_set.test_data.cpu().numpy()[split:]
-        x_test = [i.ravel() for i in x_test]
+        x_test = np.ravel(x_test)
         y_test = test_set.test_labels.cpu().numpy()[split:]
 
         train_data = (x,y)
         dev_data = (x_dev, y_dev)
         test_data = (x_test, y_test)
+        
         opt.state_size = len(set(y))
         opt.data_len = len(x)
+        
         return train_data, dev_data, test_data
